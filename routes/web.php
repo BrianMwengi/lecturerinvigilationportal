778a4cd9c7f5858eb2f88b0189e7938180129a68
Brian Mwengi <mwengibrian@gmail.com>
2026-08-13 11:07:15 +0300

    Update with a more concise route definition for the root URL and added a POST route for searching schedules.

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+Route::get('/', fn () => view('welcome'));
+
+Route::post('/search', [ScheduleController::class, 'search'])->name('schedule.search');
